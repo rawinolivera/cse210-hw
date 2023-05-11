97c8ad2e727ce8981305663f1574b0d1abc7896b
Rawin <oli18001@byui.edu>
2023-05-11 11:55:18 -0500

    Learning Activity 2 - Job Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Support Specialist";
+        job1._company = "ASOV";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
+
+        //job1.DisplayJob();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistant";
+        job2._company = "ANKA ANDINA";
+        job2._startYear = 2016;
+        job2._endYear = 2022;
+
+        //job2.DisplayJob();
+        Console.WriteLine("");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("");
+
+        Resume resume = new Resume();
+        resume._personName = "Anthony Rafael";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+        Console.WriteLine();
     }
 }
