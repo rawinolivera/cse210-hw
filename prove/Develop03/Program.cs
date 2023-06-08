5daea8e5397b999e9d2f7d943b734c6b83b2ce15
Rawin <oli18001@byui.edu>
2023-06-08 12:52:31 -0500

    defining classes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Referenc refer = new Referenc();
+        Console.WriteLine(refer.GetScriptureRef());
     }
 }
